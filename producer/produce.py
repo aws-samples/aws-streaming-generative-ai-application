b18bde0dc85b1762f4aaa86fcf2b083be7770920
meilipfi <48184762+meilipfi@users.noreply.github.com>
2024-06-24 11:40:10 +0200

    Update produce.py
    update producer to work with large movie dataset

--- producer/produce.py
+++ producer/produce.py
@@ -1,10 +1,15 @@
 import boto3
 import json
 import time
+import os
+import glob
+import random
+import uuid
 import gzip
 from botocore.exceptions import ClientError
 
-DATA_DIR = '../data/Movies_and_TV_5.json.gz'
+
+DATA_DIR = '../data/aclImdb/train/'
 AWS_REGION = 'us-east-1'
 STREAM_NAME = 'generative-ai-stream'
 BATCH_SIZE = 10
@@ -25,56 +30,60 @@ def get_kinesis_client(region: str):
     return kinesis_client
 
 
-def get_reviews(data_dir: str) -> list:
+def get_review_paths(data_dir: str) -> str:
     """
     Get paths to review files.
 
-    Args:
         data_dir (str): Directory path containing review files
 
     Returns:
-        reviews_paths (list): List of review file paths
+        review_paths (list): List of paths to review files
     """
+
+    neg_review_paths = glob.glob(os.path.join(data_dir, 'neg', '*.txt'))
+    post_review_paths = glob.glob(os.path.join(data_dir, 'pos', '*.txt'))
     
-    reviews_paths = []
+    # Combine both files and shuffle 
+    review_paths = neg_review_paths + post_review_paths
+    random.shuffle(review_paths)
 
-    try: 
-        reviews_paths = gzip.open(data_dir, 'r')
-    except FileNotFoundError:
-            print('Error: File not found.')
-
-    return reviews_paths
+    return review_paths
 
 
 def main():
     """Main function of the program. """
 
     client = get_kinesis_client(AWS_REGION)
-    reviews_paths = get_reviews(DATA_DIR)
+    review_paths = get_review_paths(DATA_DIR)
     index = 0
     review_batch = []
 
-    for review_path in reviews_paths:
-
-        review_json = json.loads(review_path)
+    # Go over all file paths
+    for review_path in review_paths: 
 
         index += 1
 
         # Retrieve review data
+        try: 
+            with open(review_path, 'r', encoding='utf-8') as file:
+                review_text = file.read()
+        except FileNotFoundError:
+                print('Error: File not found.')
+
         review = {
-            'user_id': review_json.get('reviewerID'),
+            'user_id': str(uuid.uuid4()),
             'review_id': index,
             'date_time': round(time.time()*1000),
-            'text': review_json.get('reviewText')
+            'text': review_text
         }
 
         review_batch.append(
-            {
-            'Data': json.dumps(review),
-            'PartitionKey': str(index)
-            }
-        )
-    
+                    {
+                    'Data': json.dumps(review),
+                    'PartitionKey': str(index)
+                    }
+                )
+            
         if index % BATCH_SIZE == 0:
 
             try:
@@ -95,4 +104,3 @@ def main():
         
 if __name__ == '__main__':
     main()
-
